130657739e531a95de1a05c75626983756fd0d72
xdaniel <xdanieldzd@users.noreply.github.com>
2016-02-19 02:31:46 +0100

    Improved STEX handling
    Rewritten part of STEX parser, mainly to better accommodate files with uncommon/irregular headers; minor updates to field names

--- Tharsis/FileFormats/STEX.cs
+++ Tharsis/FileFormats/STEX.cs
@@ -16,14 +16,20 @@ namespace Tharsis.FileFormats
     public class STEX : BaseFile
     {
         public string MagicNumber { get; private set; }
-        public uint Unknown1 { get; private set; }
-        public uint Unknown2 { get; private set; }
+        public uint Unknown0x04 { get; private set; } /* TODO: sometimes one, mostly zero */
+        public uint Constant3553 { get; private set; }
         public uint Width { get; private set; }
         public uint Height { get; private set; }
         public Pica.DataTypes DataType { get; private set; }
         public Pica.PixelFormats PixelFormat { get; private set; }
         public uint NumImageBytes { get; private set; }
+
+        /* Only in "xx80" STEXs */
         public uint ImageOffset { get; private set; }
+        public uint Unknown0x24 { get; private set; }
+        public string Name { get; private set; }
+
+        public byte[] RawPixelData { get; private set; }
 
         public Bitmap Image { get; private set; }
 
@@ -34,21 +40,35 @@ namespace Tharsis.FileFormats
             // "E:\[SSD User Data]\Downloads\EOIV\romfs" --ascii --colorindex --keep --output "E:\[SSD User Data]\Downloads\EOIV\dump"
 
             MagicNumber = Encoding.ASCII.GetString(reader.ReadBytes(4), 0, 4);
-            Unknown1 = reader.ReadUInt32();
-            Unknown2 = reader.ReadUInt32();
+            Unknown0x04 = reader.ReadUInt32();
+            Constant3553 = reader.ReadUInt32();
             Width = reader.ReadUInt32();
             Height = reader.ReadUInt32();
             DataType = (Pica.DataTypes)reader.ReadUInt32();
             PixelFormat = (Pica.PixelFormats)reader.ReadUInt32();
             NumImageBytes = reader.ReadUInt32();
-            ImageOffset = reader.ReadUInt32();
 
-            reader.BaseStream.Seek(0x28, SeekOrigin.Begin);
-            if (reader.ReadUInt32() != 0 || reader.ReadUInt32() != 0) ImageOffset = 0x20;
+            if ((reader.BaseStream.Length & 0x000000FF) == 0x00 || (reader.BaseStream.Length & 0x000000FF) == 0x80)
+            {
+                ImageOffset = reader.ReadUInt32();
+                Unknown0x24 = reader.ReadUInt32();
+                Name = Encoding.ASCII.GetString(reader.ReadBytes(0x58), 0, 0x58).TrimEnd('\0');
 
-            reader.BaseStream.Seek(ImageOffset, SeekOrigin.Begin);
+                reader.BaseStream.Seek(ImageOffset, SeekOrigin.Begin);
+                RawPixelData = reader.ReadBytes((int)(NumImageBytes > reader.BaseStream.Length ? reader.BaseStream.Length - ImageOffset : NumImageBytes));
+            }
+            else if ((reader.BaseStream.Length & 0x000000FF) == 0x20)
+            {
+                ImageOffset = (uint)reader.BaseStream.Position;
+                Unknown0x24 = uint.MaxValue;
+                Name = string.Empty;
 
-            Image = Texture.ToBitmap(DataType, PixelFormat, (int)Width, (int)Height, reader);
+                RawPixelData = reader.ReadBytes((int)(reader.BaseStream.Length - reader.BaseStream.Position));
+            }
+            else
+                throw new NotImplementedException("Malformed or unrecognized STEX header; please report");
+
+            Image = Texture.ToBitmap(DataType, PixelFormat, (int)Width, (int)Height, RawPixelData);
         }
 
         public override bool Save(string path)
