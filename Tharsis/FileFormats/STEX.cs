1be57c3dc70c8a3ebfc147bfb285d84e3180c355
xdaniel <xdanieldzd@users.noreply.github.com>
2016-03-03 03:09:30 +0100

    STEX parser rework
    Reworked the STEX parser *again* to compensate for the format's unreliability; making certain hacky assumptions, but it's the only way I can think of for it to read the various odd STEXs out there...

--- Tharsis/FileFormats/STEX.cs
+++ Tharsis/FileFormats/STEX.cs
@@ -22,12 +22,12 @@ namespace Tharsis.FileFormats
         public uint Height { get; private set; }
         public Pica.DataTypes DataType { get; private set; }
         public Pica.PixelFormats PixelFormat { get; private set; }
-        public uint NumImageBytes { get; private set; }
+        public uint NumImageBytes { get; private set; } /* Unreliable! */
 
         /* Only in "xx80" STEXs */
         public uint ImageOffset { get; private set; }
         public uint Unknown0x24 { get; private set; }
-        public string Name { get; private set; }
+        public string Name { get; private set; } /* Only in newer STEX? (ex. SMT4F) */
 
         public byte[] RawPixelData { get; private set; }
 
@@ -48,25 +48,35 @@ namespace Tharsis.FileFormats
             PixelFormat = (Pica.PixelFormats)reader.ReadUInt32();
             NumImageBytes = reader.ReadUInt32();
 
-            if ((reader.BaseStream.Length & 0x000000FF) == 0x00 || (reader.BaseStream.Length & 0x000000FF) == 0x80)
+            /* Disclaimer: Hacky as hell! I almost want to hope that Atlus someday leaves their internal STEX creator tool inside one of these games, maybe that'll help with figuring this out <.< */
+
+            /* ...now at offset 0x20, assume here's the pointer to image data */
+            ImageOffset = reader.ReadUInt32();
+
+            /* ...now assume said pointer is 0x80 */
+            if (ImageOffset == 0x80)
             {
-                ImageOffset = reader.ReadUInt32();
+                /* Read "well-formed" STEX (but really, who knows how the header's supposed to be) */
                 Unknown0x24 = reader.ReadUInt32();
                 Name = Encoding.ASCII.GetString(reader.ReadBytes(0x58), 0, 0x58).TrimEnd('\0');
 
+                /* ...but as image datasize is also unreliable, do some additional sanity checking on that, too! */
                 reader.BaseStream.Seek(ImageOffset, SeekOrigin.Begin);
                 RawPixelData = reader.ReadBytes((int)(NumImageBytes > reader.BaseStream.Length ? reader.BaseStream.Length - ImageOffset : NumImageBytes));
             }
-            else if ((reader.BaseStream.Length & 0x000000FF) == 0x20)
+            else /* ...otherwise... */
             {
+                /* Seek back, then just assume image data starts right here at 0x20, and that the image is as many bytes long as are left in the file */
+                reader.BaseStream.Seek(-4, SeekOrigin.Current);
+
                 ImageOffset = (uint)reader.BaseStream.Position;
                 Unknown0x24 = uint.MaxValue;
                 Name = string.Empty;
 
                 RawPixelData = reader.ReadBytes((int)(reader.BaseStream.Length - reader.BaseStream.Position));
             }
-            else
-                throw new NotImplementedException("Malformed or unrecognized STEX header; please report");
+
+            /* I hope this covers every STEX out there right now. If not... I'm running out of ideas */
 
             Image = Texture.ToBitmap(DataType, PixelFormat, (int)Width, (int)Height, RawPixelData);
         }
